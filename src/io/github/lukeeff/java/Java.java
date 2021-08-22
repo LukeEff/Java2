fb89e025263a9d7e607d4d149664116d195982e7
lukeeff <luke.matthew.fernandez@gmail.com>
2021-08-22 16:06:39 -0700

    Initial commit

---
+++ src/io/github/lukeeff/java/Java.java
@@ -0,0 +1,11 @@
+package io.github.lukeeff.java;
+
+import io.github.lukeeff.java.people.Person;
+
+public class Java {
+
+    public static void main(String[] args) {
+        Person person = new Person("Luke");
+        System.out.println(person.getName());
+    }
+}
---
+++ src/io/github/lukeeff/java/people/Person.java
@@ -0,0 +1,14 @@
+package io.github.lukeeff.java.people;
+
+public class Person {
+
+    private String name;
+
+    public Person(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+}
