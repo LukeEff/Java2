008964e7930e130a21ea41c068d0bac9740b742f
lukeeff <luke.matthew.fernandez@gmail.com>
2021-08-22 17:06:16 -0700

    Add public access modifier to EyeColor

--- src/io/github/lukeeff/java/Java.java
+++ src/io/github/lukeeff/java/Java.java
@@ -1,11 +1,12 @@
 package io.github.lukeeff.java;
 
 import io.github.lukeeff.java.people.Person;
+import io.github.lukeeff.java.people.Person.EyeColor;
 
 public class Java {
 
     public static void main(String[] args) {
-        Person person = new Person("Luke", 20);
+        Person person = new Person("Luke", 20, EyeColor.BROWN);
         String reversedName = StringUtil.reverse(person.getName());
 
         System.out.println(reversedName);
--- src/io/github/lukeeff/java/people/Person.java
+++ src/io/github/lukeeff/java/people/Person.java
@@ -4,19 +4,21 @@ public class Person {
 
     private String name;
     private int age;
+    private EyeColor eyeColor;
 
     private static final int OLD_AGE = 70;
 
-    enum EyeColor {
+    public enum EyeColor {
         BROWN(),
         HAZEL(),
         BLUE(),
         GREEN();
     }
 
-    public Person(String name, int age) {
+    public Person(String name, int age, EyeColor eyeColor) {
         this.name = name;
         this.age = age;
+        this.eyeColor = eyeColor;
     }
 
     public String getName() {
